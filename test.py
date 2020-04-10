e8c0197ee016656492fab6b9f3e47814f8202f29
bratiu <ratiubogdan@yahoo.com>
2020-04-10 15:40:58 +0200

    Small Changes

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
+# import sys and os just to be able to clear the screen between questions.
+# sys to check the operating system and os to call the proper clear screen function
 import sys
 import os
+
+# Import the test_questions in which we have the text of the questions and the answers
 import test_questions
 
+
+# This function will just clear the screen between each question
 def clear_screen():
     print(sys.platform)
     if sys.platform == "win32":
@@ -11,12 +17,14 @@ def clear_screen():
         os.system("clear")
 
 
+# Count the correct number of answers
 correct = 0
 for i in range(10):
     clear_screen()
+    print("What will the below code print?\nPlease choose the correct answer (type a,b,c or d)")
     print(test_questions.questions[i])
     answer = input("What is your answer? ")
     if answer == test_questions.answers[i]:
         correct += 1
 
-print("your final score is: {}/10".format(correct))
+print("your final score is: {}/{}".format(correct, len(test_questions.questions)))
--- test_questions.py
+++ test_questions.py
@@ -1,14 +1,17 @@
-# just define simple test questions (the question + possible answers)
-# the questions are defined as a list in which we keep adding text.
-# This program does not assume anything regarding the number of questions
+# This is to "hide" the correct answers into a different file
+# The questions are defined into the questios.txt file
+# We parse this file and construct a list of questions, plus I define a matching list of correct answers
 
+# These are the correct answers
 answers = ["c", "d", "c", "b", "a", "d", "c", "c", "b", "b"]
 
-# The questions are in a list
+# The questions are in a file that we open and read from
 fp = open("questions.txt")
 all_text = fp.read()
 
-
-# define the questions list
+# We parse the file and get the list of questions
 questions = all_text.split("\n\n\n")
+
+# Once we are done, we close the file
+fp.close()
 
